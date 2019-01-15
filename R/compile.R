fd5f232d9e0f020a5587e6569a7df6102249ebc7
Alex Pellett <PellettC@dnr.sc.gov>
2019-01-15 17:36:00 -0500

    keep working on integrating old code in to a package

---
+++ R/compile.R
@@ -0,0 +1,35 @@
+###
+monthyear_to_date <- function(df, monthcol='Month', yearcol='Year', datecol='Date') {
+  months <- df[,monthcol, drop=TRUE]
+  months[is.na(months)] <- 12
+  years <- df[,yearcol, drop=TRUE]
+  df[, datecol] <- as_date(as.yearmon(
+    paste0(months, ' ', years),
+    format="%m %Y"), frac=1)
+  df[, !(names(df) %in% c(monthcol, yearcol))]
+}
+
+# test <- monthyear_to_date(eia_generator, 'Operating Month', 'Operating Year', 'Operating Date')
+
+###
+join_and_drop <- function(df1, df2, by=NULL, rename=FALSE) {
+  df <- left_join(df1, df2, by)
+  if(is.null(by)){
+    by <- names(df1)[names(df1) %in% names(df2)]
+  }
+  df <- df[,!(names(df) %in% c(by, names(by)) ) ]
+  if(is.character(rename)) {
+    names(df)[(length(df)-(length(rename)-1)):length(df)] <- rename
+  }
+  df
+}
+
+paste_columns <- function(df, columns, new_col='new_col', collapse='; ') {
+  columns <- df[, columns]
+  df <- df[, !(names(df) %in% names(columns))]
+  columns <- columns %>% rowwise() %>%
+    do( new_col = unique(.) %>% na.omit() %>%
+          paste0(collapse=collapse) )
+  df[, new_col] <- columns[,'new_col', drop=TRUE] %>% unlist()
+  df
+}
